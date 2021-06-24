9f63ddfc6c67ec98aea1f51874d8c6d29255b334
Jearce <jessiearce42@gmail.com>
2021-06-23 21:01:12 -0500

    test for pilon subcommand

--- test_cmd_app/test_ares.py
+++ test_cmd_app/test_ares.py
@@ -5,9 +5,19 @@ import subprocess
 from pathlib import Path
 
 
-def test_areas():
+def test_ares():
     p = subprocess.Popen(
         "python ares.py test_data/drafts test_data/long.fastq test_data/r1.fastq test_data/r2.fastq fungi_odb10 30".split()
+    )
+    stdout, stderr = p.communicate()
+    assert p.returncode == 0
+    assert os.path.exists("ares_out")
+    assert os.path.exists("ares_out/best/polish.fasta")
+
+
+def test_pilon():
+    p = subprocess.Popen(
+        "python ares.py pilon test_data/drafts test_data/r1.fastq test_data/r2.fastq fungi_odb10 30".split()
     )
     stdout, stderr = p.communicate()
     assert p.returncode == 0
