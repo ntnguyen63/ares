76e3f4803295c5af7ea518eb03f35cf2ed42bff7
Jearce <jessiearce42@gmail.com>
2021-06-23 23:50:00 -0500

    finished test for pilon subcommand

--- test_cmd_app/test_ares.py
+++ test_cmd_app/test_ares.py
@@ -17,9 +17,9 @@ def test_ares():
 
 def test_pilon():
     p = subprocess.Popen(
-        "python ares.py pilon test_data/drafts test_data/r1.fastq test_data/r2.fastq fungi_odb10 30".split()
+        "python ares.py pilon pilon_command_test test_data/drafts/assembly.fasta test_data/r1.fastq test_data/r2.fastq fungi_odb10 30".split()
     )
     stdout, stderr = p.communicate()
     assert p.returncode == 0
-    assert os.path.exists("ares_out")
-    assert os.path.exists("ares_out/best/polish.fasta")
+    assert os.path.exists("pilon_command_test")
+    assert os.path.exists("pilon_command_test/best/polish.fasta")
